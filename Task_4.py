5902bc342a8170448d2159705a6aa6e309ae069f
KarenRouge <karenrouge1991@gmail.com>
2021-11-17 15:40:03 -0500

    Task_4 update

--- Task_4.py
+++ Task_4.py
@@ -1,12 +1,10 @@
 words = input("Введите список продуктов через пробел: ").split()
-print(words)
 words = list(words)
 len_words = [len(words[i]) for i in range(0, len(words))]
-print(len_words)
-i=0
+i = 0
 while len(words[i]) <= 10:
-     i = i + 1
-     if i == len(words):
+    i = i + 1
+    if i == len(words):
         break
 k = i
 if len(words[k]) <= 10:
@@ -14,6 +12,10 @@ if len(words[k]) <= 10:
         print(ind, words.format)
 else:
     long_word = words[k]
-print(long_word)
-
-
+words.pop(k)
+long_word_cut = long_word[0:9]
+words.insert(k, long_word_cut)
+words = list(words)
+print(words)
+for ind, words in enumerate(words, 1):
+    print(ind, words.format)
