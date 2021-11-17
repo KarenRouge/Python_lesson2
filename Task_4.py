3cf07f71d3f86955ff7432ac7ab1a017ae964f04
KarenRouge <karenrouge1991@gmail.com>
2021-11-17 14:33:35 -0500

    TAsk_4 update

--- Task_4.py
+++ Task_4.py
@@ -3,15 +3,17 @@ print(words)
 words = list(words)
 len_words = [len(words[i]) for i in range(0, len(words))]
 print(len_words)
-for ind, words in enumerate(words, 1):
-    print(ind, words)
-
-
-
-
-
-
-
-
+i=0
+while len(words[i]) <= 10:
+     i = i + 1
+     if i == len(words):
+        break
+k = i
+if len(words[k]) <= 10:
+    for ind, words in enumerate(words, 1):
+        print(ind, words.format)
+else:
+    long_word = words[k]
+print(long_word)
 
 
