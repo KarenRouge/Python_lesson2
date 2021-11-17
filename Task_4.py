4b4a89f3b05014a812783301b17d70c4f77b66de
KarenRouge <karenrouge1991@gmail.com>
2021-11-17 13:41:23 -0500

    Task_4 update

--- Task_4.py
+++ Task_4.py
@@ -1,1 +1,17 @@
-words = input("Введите список продуктов через пробел: ")
+words = input("Введите список продуктов через пробел: ").split()
+print(words)
+words = list(words)
+len_words = [len(words[i]) for i in range(0, len(words))]
+print(len_words)
+for ind, words in enumerate(words, 1):
+    print(ind, words)
+
+
+
+
+
+
+
+
+
+
